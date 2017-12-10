3a2a7658888cefde2704a4dd74dc795aa0f341e4
Your Name <damnit1989@163.com>
2017-12-10 03:26:52 -0800

    mod

--- redis_publis.php
+++ redis_publis.php
@@ -7,5 +7,16 @@ $task = array(
 	'do' => 'test',
 	'data' => 'hello world'
 );
+
 #$redis->publish('take_queue',serialize($task)); 
 $redis->publish('test1',serialize($task)); 
+
+$task_send_email = array(
+	'do' => 'send_email',
+	'data' => array(
+		'email' => 'bitch1989@163.com',
+		'message' => 'hello world'
+	)
+)
+
+$redis->publish('test1',serialize($task_send_email));
