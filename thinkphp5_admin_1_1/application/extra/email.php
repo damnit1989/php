e77d06e8ef227a56ee647bdca802d8e2666d56dc
Your Name <damnit1989@163.com>
2018-02-12 23:32:55 -0800

    edit

--- thinkphp5_admin_1_1/application/extra/email.php
+++ thinkphp5_admin_1_1/application/extra/email.php
@@ -7,9 +7,9 @@
 return [
     'host' => 'smtp.163.com', // 所使用的邮箱smtp地址
     'port' => 465, // 端口号25, ssl端口: 465,994
-    'username' => 'lmm-3971655@163.com', // 发送邮件的邮箱
-    'password' => 'bitch1989', //密码或者授权码,授权码可以防止密码泄露
+    'username' => 'xxxxxxx@163.com', // 发送邮件的邮箱
+    'password' => 'xxxxxxx', //密码或者授权码,授权码可以防止密码泄露
     // 'password' => 'xxxxxx', // 授权密码
     'charset' => 'utf-8', // 编码格式
-	'name' => '幂聚科技(北京)有限公司'
+	'name' => '本人'
 ];
